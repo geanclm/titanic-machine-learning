795a281767261394233d26e557404b7bed8cf2b4
Gean Carlos <geanclm@gmail.com>
2024-01-16 20:24:13 -0300

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -21,8 +21,8 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 # from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.model_selection import train_test_split
+# from sklearn.metrics import confusion_matrix, accuracy_score
+# from sklearn.model_selection import train_test_split
 
 # st.set_page_config(page_title="Predição de sobreviventes do Titanic")
 # with st.container():
